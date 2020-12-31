8d5289cd18eba884a8cd12faf7c530d781c68346
ale <malejlopez@hotmail.com>
2020-12-31 00:10:53 -0600

    Simple Debuggin in Javascript

---
+++ debugging_in_js/notes.js
@@ -0,0 +1,17 @@
+// Type Of
+//JavaScript recognizes six primitive (immutable) data types: Boolean, Null, Undefined, Number, String, and Symbol
+
+//syntax
+// typeof something
+//something literal or a variable
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+
+
+console.log(typeof seven);//number
+console.log(typeof (three));//string
+console.log(typeof (seven + three));//string
+//adding a string and number appends in string style
+
+// When misspelling it will intrepe it as an object that does not exist
