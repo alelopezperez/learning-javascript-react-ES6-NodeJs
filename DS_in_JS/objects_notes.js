a493d53739bae0cb101b0640f903d3b36a27e727
ale <malejlopez@hotmail.com>
2021-01-06 18:45:54 -0600

    Started Objects in Basic Ds in JS

---
+++ DS_in_JS/objects_notes.js
@@ -0,0 +1,17 @@
+/* Key Value Pairs in JavaScript Objects
+	
+*/
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+
+foods.bananas=13;
+foods['grapes']=35;
+foods["strawberries"]=27;
+
+
+console.log(foods);
