155cb347c550171fff342be27476c1dc11f2bea0
ale <malejlopez@hotmail.com>
2021-01-06 18:35:49 -0600

    Basic DS in JS almost finished

--- DS_in_JS/array_notes.js
+++ DS_in_JS/array_notes.js
@@ -34,4 +34,109 @@ function popShift(arr) {
   return [shifted, popped];// [challenge,complete]
 }
 
+/* Remove Items
+	splice()
+- First Argument Index of the begning
+- Amount of elemnt to be removed including the begenning
+- It Returns the removed elements.
+*/	
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+
+arr.splice(4);// Only one argument it will keep until it reaches the final element.
+arr.splice(0,1);
+console.log(arr);
+//[4, 5, 1]
+
+
+/* Add Elements
+	splice()
+	
+The Third element an onward it's to add any item from the beggining index argument(1st arguemnt).
+
+*/
+
+function htmlColorNames(arr) {
+  arr.splice(0,2, 'DarkSalmon','BlanchedAlmond');
+  return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+/* Copy Elements
+	slice()
+	
+slice will take only two arguments intial pos(inclusive), and ending post (exclusive) and copy it to a new array and return it.
+it will leave the original array unmodified(functional programming)
+*/
+
+function forecast(arr) {  
+  return arr.slice(2,4);
+}
+//if you dont specify the second argument it will continue right until the the last element
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms'])); //[warm, sunny]
+
+
+/* Copy Elements
+	spread operator
+	takes an array an breaks it down to its element so it can be copied
+	it can be used inside the creation of an array
+
+*/
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    let newArr1= [...arr];
+    num--;
+    newArr.push(newArr1);
+  }
+  return newArr;
+}
+console.log(copyMachine([true, false, true], 2));
+
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ['learning', ...fragment, 'is', 'fun']; 
+  let sentence = ['learning', [...fragment], 'is', 'fun']; 
+  return sentence;
+}
+
+console.log(spreadOut());
+
+
+/* Search Elements in an Array
+
+	indexOf();
+	it's only the first instance.
+*/
+
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+
+fruits.indexOf('dates'); // returns -1
+fruits.indexOf('oranges'); // returns 2
+fruits.indexOf('pears'); // returns 1, the first index at which the element exists
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
