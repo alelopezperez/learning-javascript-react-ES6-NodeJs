3936fc049c097c018ed9c9e3e71afcf40a3233a4
ale <malejlopez@hotmail.com>
2021-02-03 17:49:58 -0600

    Finished Section 9 Udemy

--- Learning_react/Udemy/pics/src/components/App.js
+++ Learning_react/Udemy/pics/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash.js';
 import SearchBar from './SearchBar.js';
+import ImageList from './ImageList'
 
 class App extends React.Component {
 	state = { images: [] };
@@ -20,10 +21,11 @@ class App extends React.Component {
 					onSubmit={this.onSearchSubmit}
 					onSecond={this.onSecond}
 				/>
-				Found {this.state.images.length} images
+				<ImageList images = {this.state.images}/>
 			</div>
 		);
 	}
 }
 
 export default App;
+
