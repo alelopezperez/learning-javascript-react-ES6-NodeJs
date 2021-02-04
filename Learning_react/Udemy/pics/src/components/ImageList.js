7486b65469be5d45c5501165446bcae68c3413b9
ale <malejlopez@hotmail.com>
2021-02-04 16:53:23 -0600

    Finished Section 10 React Udemy

--- Learning_react/Udemy/pics/src/components/ImageList.js
+++ Learning_react/Udemy/pics/src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard.js';
 
-const ImageList = (props) => {
-	const images = props.images.map(({id, description, urls})=>{
-		return <img key={id}  alt={description} src={urls.regular} />	
-	})
+const ImageList = props => {
+	const images = props.images.map(image => {
+		return <ImageCard key={image.id} image={image} />;
+	});
 
-	return <div>{images}</div>;
+	return <div className="image-list">{images}</div>;
 };
- 
-export default  ImageList;
+
+export default ImageList;
