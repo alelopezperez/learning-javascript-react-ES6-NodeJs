2f0f225d393db09c40bf7eec5b04f7565d793757
ale <malejlopez@hotmail.com>
2021-02-11 20:01:10 -0600

    Finished Section 13 Udemy React

--- Learning_react/Udemy/widgets/src/App.js
+++ Learning_react/Udemy/widgets/src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion.js';
 import Search from './components/Search.js';
 import Dropdown from './components/Dropdown.js';
-import Translate from './components/Translate.js'
+import Translate from './components/Translate.js';
+import Route from './components/Route.js';
+import Header from './components/Header.js';
+
 const items = [
 	{
 		title: 'What is React?',
@@ -34,10 +37,30 @@ const options = [
 ];
 
 const App = () => {
-
+	const [selected, setSelected] = useState(options[0]);
 	return (
 		<div>
-			<Translate />
+			<Header />
+			<Route path="/">
+				<Accordion items={items} />
+			</Route>
+
+			<Route path="/list">
+				<Search />
+			</Route>
+
+			<Route path="/dropdown">
+				<Dropdown
+					label={'Select A Color'}
+					options={options}
+					selected={selected}
+					onSelectedChange={setSelected}
+				/>
+			</Route>
+
+			<Route path="/translate">
+				<Translate />
+			</Route>
 		</div>
 	);
 };
---
+++ Learning_react/Udemy/widgets/src/components/Link.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Link = ({ href, className, children }) => {
+	const onClick = event => {
+		if (event.metakey || event.ctrlKey) return;
+
+		event.preventDefault();
+		window.history.pushState({}, '', href);
+
+		const navEvent = new PopStateEvent('popstate');
+		window.dispatchEvent(navEvent);
+	};
+	return (
+		<a onClick={onClick} className={className} href={href}>
+			{children}
+		</a>
+	);
+};
+
+export default Link;
