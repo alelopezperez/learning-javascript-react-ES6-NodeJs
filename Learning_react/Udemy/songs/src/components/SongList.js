90be8f07938880b498c852434e50dd6332797de3
ale <malejlopez@hotmail.com>
2021-02-19 18:28:49 -0600

    Finished Section 17 Udemy React

--- Learning_react/Udemy/songs/src/components/SongList.js
+++ Learning_react/Udemy/songs/src/components/SongList.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
 	renderList() {
 		return this.props.songs.map(song => {
 			return (
-				<div className='item' key={song.title}>
-					<div className='right floated content'>
-						<button className='ui button primary'>
+				<div className="item" key={song.title}>
+					<div className="right floated content">
+						<button
+							className="ui button primary"
+							onClick={() => this.props.selectSong(song)}
+						>
 							Select
 						</button>
 					</div>
-					<div className='content'>{song.title}</div>
+					<div className="content">{song.title}</div>
 				</div>
 			);
 		});
 	}
 	render() {
-		return <div className='ui divided list'>{this.renderList()}</div>;
+		return <div className="ui divided list">{this.renderList()}</div>;
 	}
 }
 
@@ -25,4 +29,4 @@ const mapStateToProps = state => {
 	return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
