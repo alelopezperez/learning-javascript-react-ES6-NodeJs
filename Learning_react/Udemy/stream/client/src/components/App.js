c64cc9dcfd00b99a0ffc36a76c2250def6af9b74
ale <malejlopez@hotmail.com>
2021-03-02 21:06:16 -0600

    React Udemy Section 21 Finished

--- Learning_react/Udemy/stream/client/src/components/App.js
+++ Learning_react/Udemy/stream/client/src/components/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate.js';
 import StreamDelete from './streams/StreamDelete.js';
 import StreamEdit from './streams/StreamEdit.js';
 import StreamList from './streams/StreamList.js';
 import StreamShow from './streams/StreamShow.js';
-import Header from './Header.js'
+import Header from './Header.js';
 
 const App = () => {
 	return (
-		<div className='ui container'>
+		<div className="ui container">
 			<BrowserRouter>
 				<div>
 					<Header />
 					<Route path="/" exact component={StreamList} />
 					<Route path="/streams/new" exact component={StreamCreate} />
 					<Route path="/streams/edit" exact component={StreamEdit} />
-					<Route path="/streams/delete" exact component={StreamDelete} />
+					<Route
+						path="/streams/delete"
+						exact
+						component={StreamDelete}
+					/>
 					<Route path="/streams/show" exact component={StreamShow} />
 				</div>
 			</BrowserRouter>
