169e504c4cf9df3a627ea1b4ddb3e8d54c085526
ale <malejlopez@hotmail.com>
2021-03-30 20:03:38 -0600

    Udemy React Section 24 11/35

--- Learning_react/Udemy/stream/client/src/index.js
+++ Learning_react/Udemy/stream/client/src/index.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App.js';
 import reducers from './reducers';
+import reduxThunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
 	<Provider store={store}>
