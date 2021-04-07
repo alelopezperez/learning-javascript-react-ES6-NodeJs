b0f8412b304d8720f70c29cdc77765d4a301365d
ale <malejlopez@hotmail.com>
2021-04-06 19:46:17 -0500

    Section 27 Finished Udemy React

--- Learning_react/Udemy/stream/client/src/components/streams/StreamList.js
+++ Learning_react/Udemy/stream/client/src/components/streams/StreamList.js
@@ -18,7 +18,12 @@ class StreamList extends React.Component {
 					>
 						Edit
 					</Link>
-					<Link to={`/streams/delete/${stream.id}`} className="ui button negative">Delete</Link>
+					<Link
+						to={`/streams/delete/${stream.id}`}
+						className="ui button negative"
+					>
+						Delete
+					</Link>
 				</div>
 			);
 	}
@@ -30,7 +35,9 @@ class StreamList extends React.Component {
 					{this.renderAdmin(stream)}
 					<i className="large middle aligned icon camera" />
 					<div className="content">
-						{stream.title}
+						<Link to={`/streams/${stream.id}`} className="header">
+							{stream.title}
+						</Link>
 						<div className="description">{stream.description}</div>
 					</div>
 				</div>
